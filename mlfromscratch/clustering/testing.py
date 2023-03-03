c4ae62a58ff2281b1d4eda1ebacc83bd595873ac
jafar_bakhshaliyev <jafar.bakhshaliyev@gmail.com>
2023-03-03 23:12:34 +0100

    updated

--- mlfromscratch/clustering/testing.py
+++
@@ -1,5 +0,0 @@
-from GMM import GaussianMixtureModel
-import sys
-sys.path.append("/Users/jafar_bakhshaliyev/ML-from-Scratch/ML-from-Scratch/ML-from-Scratch/mlfromscratch/")
-
-from utils.preprocessing import StandardScaler
