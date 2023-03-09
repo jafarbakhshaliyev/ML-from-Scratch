8bb41c9a685ceeb6f6fc8da89d583b9928349081
jafar_bakhshaliyev <jafar.bakhshaliyev@gmail.com>
2023-03-09 23:27:07 +0100

    updated loss

--- mlfromscratch/losses/loss.py
+++ mlfromscratch/losses/loss.py
@@ -1,4 +1,19 @@
 import numpy as np
 
 class Loss:
-    pass
+    def __init__(self) -> None:
+       pass
+    
+    def mse(self, X, y, theta):
+       return ((X@theta-y)**2).mean(), 1.0/len(y)*X.T.dot(X.dot(theta) - y), 1.0/len(y)*X.T@X
+    
+    def binary_cross_entropy(self, X, y, theta):
+       
+       epsilon = 1e-9
+       p = 1/(1+np.exp(-X@theta))
+       p = np.clip(p, epsilon, 1. - epsilon) 
+       W = np.diag((p*(1-p)).reshape(-1))
+       
+       return -1.0/len(y)*(y.T@np.log(p) + (1-y).T@np.log((1-p))),-1.0/len(y)*X.T.dot(y - p), 1.0/len(y)*X.T@W@X
+
+ 
